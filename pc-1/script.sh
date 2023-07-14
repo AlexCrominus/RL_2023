867e0fb7f369b55b2b1971cbac138b5aa8837e59
cr0minus <antal7349@gmail.com>
2023-07-14 18:56:00 +0300

    ftp now works, cant get systemctl working

--- pc-1/script.sh
+++ pc-1/script.sh
@@ -1,3 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "ce faceti prostilor" > /tmp/idk
+mkdir -p /home/ftp/...
+
+echo "sunt asa de prost inat mi-am uitat cheia de la serverul de ssh, sper sa ma poti ajuta sa o gasesc" > /home/ftp/notes  
+echo "ce faci prostule" > /home/ftp/.../idk
+
+/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
